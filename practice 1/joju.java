1865142a82e3dc1bbb02d1ae36ffdc0d7775b371
akarlin19032006-ctrl <a.karlin19032006@gmail.com>
2025-09-29 14:45:41 +0300

    Add files via upload

---
+++ practice 1/joju.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        # Создание массива типа список
+        List<String> lzl = new ArrayList<>();
+        aList.add("nom");
+        aList.add("loap");
+        aList.add("zoia");
+
+        # Организация стека
+        Stack<String> names = new Stack<>();
+
+        # Добавление в стек
+        stack.push("bil");    
+        stack.push("oil");    
+        stack.push("c");  
+  
+        System.out.println("Initial Stack: " + names);
+
+        # Удаление из стека
+        String name = names.pop();  
+
+        System.out.println("Popped name: " + name);
+        System.out.println("Stack after pop: " + names);
+    }
+}
